0e9e2925fbf649a2351b925a5f02e174cfd1ea18
Arian <arianmohseni1382@gmail.com>
2023-03-30 15:56:15 +0430

    fields added

--- 05/vote.java
+++ 05/vote.java
@@ -4,7 +4,26 @@ import java.util.ArrayList;
 
 
 public class vote {
+
+    public class Person {
+
+        private String firstname;
+        private String lastname;
+        
+        
+        
+        
+
+        
+
+        
+        
+
+    }
+
     public static void main(String[] args) {
         
+        
+
     }
 }
