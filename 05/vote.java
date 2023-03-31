81b74b394ceea7a93fa347c391aa0e3829d204d3
Arian <arianmohseni1382@gmail.com>
2023-03-31 21:06:22 +0430

    Voting class added

--- 05/vote.java
+++ 05/vote.java
@@ -1,5 +1,70 @@
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Objects;
+
+
+class Voting {
+
+    private int type;
+    private String question;
+    private HashMap<String, HashSet<Vote>> choices;
+    private boolean isAnonymous;
+    private ArrayList<Person> voters;
+
+    
+    public String getQuestion() {
+        return question;
+    }
+
+    public ArrayList<String> getChoices() {
+        return new ArrayList<>(choices.keySet());
+    }
+
+    public Voting(int type, String question, boolean isAnonymous) {
+        this.type = type;
+        this.question = question;
+        this.isAnonymous = isAnonymous;
+        choices = new HashMap<>();
+        voters = new ArrayList<>();
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public void createChoice(String choice) {
+        choices.put(choice, new HashSet<>());
+    }
+
+    public void vote(Person voter, ArrayList<String> voter_choices){
+        if (type == 0) {
+            System.out.println("You can only select one choice for this voting.");
+        }
+        if (!voters.contains(voter)) {
+            voters.add(voter);
+        }
+        for (String choice : voter_choices) {
+            if (!choices.containsKey(choice)) {
+                System.out.println("Invalid choice: " + choice);
+            }
+
+            Vote vote = new Vote(voter, "");
+
+            if (!isAnonymous) {
+                vote = new Vote(voter, "some date");
+            }
+
+            choices.get(choice).add(vote);
+        }
+    }
+
+    
+
+
+    
+
+}
 
 
 class Vote {
