55518cea85b4a42ed8d6c2f93dee5a5c78dd5bcd
Arian <arianmohseni1382@gmail.com>
2023-03-31 12:56:18 +0430

    some changes + Vote class fields added

--- 05/vote.java
+++ 05/vote.java
@@ -1,39 +1,47 @@
 import java.util.ArrayList;
 
 
+class Vote {
+
+    private final Person voter;
+    private final String date;
+
+    
 
 
-public class vote {
 
-    public class Person {
 
-        private String firstname;
-        private String lastname;
-        
-        
-        public Person(String firstname,String lastname){
-            this.firstname = firstname;
-            this.lastname = lastname;
-        }
-        
-        public String getFirstName(){
-            return firstname;
-        }
+}
 
-        public String getLastName(){
-            return lastname;
-        }
-        
+class Person {
 
-        public String toString(){
-            return firstname + '-' + lastname;
-        }
-        
+    private String firstname;
+    private String lastname;
+    
+    
+    public Person(String firstname,String lastname){
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+    
+    public String getFirstName(){
+        return firstname;
     }
 
+    public String getLastName(){
+        return lastname;
+    }
+    
+
+    public String toString(){
+        return firstname + '-' + lastname;
+    }
+    
+}
+
+public class vote {
     public static void main(String[] args) {
         
         
-
     }
 }
