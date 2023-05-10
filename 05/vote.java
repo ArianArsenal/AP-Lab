d5ce9397f969cf2013430754316694e5f412902c
Arian <arianmohseni1382@gmail.com>
2023-05-10 14:43:14 +0330

    person class added

--- 05/vote.java
+++ 05/vote.java
@@ -77,6 +77,21 @@ class Voting {
     }
 }
 
+class Person {
+    private final String firstname;
+    private final String lastname;
+
+    public Person(String firstname , String lastname){
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    @Override
+    public String toString() {
+        return "firstname :" + firstname +
+                " , lastname : " + lastname;
+    }
+}
 
 
 class VotingSystem {
