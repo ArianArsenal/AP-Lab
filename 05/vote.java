e106f3dce2b973c948bd9e2f178c660663804943
Arian <arianmohseni1382@gmail.com>
2023-05-10 14:44:11 +0330

    Vote class added

--- 05/vote.java
+++ 05/vote.java
@@ -7,6 +7,22 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
 
+class Vote {
+    private final Person voter;
+    private final String date;
+
+    public Vote(Person voter, String date) {
+        this.voter = voter;
+        this.date = date;
+    }
+
+    public Person getVoter() {
+        return voter;
+    }
+}
+
+
+
 class Voting {
     private String question;
     private HashMap <String, HashSet<Vote>> choices;
